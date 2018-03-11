e9a55fcf634a1a3633a3fb6f587db077c1b74a04
niraqw15 <niraqw@gmail.com>
2018-03-11 16:51:20 -0700

    Add ColorCycleTask class (AsyncTask)

---
+++ app/src/main/java/com/example/sam/blutoothsocketreceiver/ColorCycleTask.java
@@ -0,0 +1,34 @@
+package com.example.sam.blutoothsocketreceiver;
+
+import android.os.AsyncTask;
+import android.text.SpannableString;
+import android.widget.TextView;
+
+/**
+ * Created by niraq on 3/11/2018.
+ */
+
+public class ColorCycleTask extends AsyncTask<Object, SpannableString, Void> {
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected Void doInBackground(Object... objects) {
+
+
+    }
+
+    @Override
+    protected void onProgressUpdate(SpannableString... strings) {
+
+    }
+
+    @Override
+    protected void onPostExecute() {
+
+    }
+
+}
